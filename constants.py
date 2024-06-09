1ff5dbeddf6d78999f7aa7f1711218ce841c60e9
jorgenwh <jorgenwhen@gmail.com>
2024-06-09 14:14:34 +0200

    srttings

--- constants.py
+++ constants.py
@@ -20,7 +20,7 @@ NUM_BINS             = 32
 
 # --- Training settings ---
 LEARNING_RATE        = 0.001
-BATCH_SIZE           = 128
+BATCH_SIZE           = 256
 TRAINING_ITERS       = 1000000
 DEVICE               = "cuda:0"
 OUTPUT_DIR           = "models"
