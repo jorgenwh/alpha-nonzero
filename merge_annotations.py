eecbbac0ebee677036fd85ac98e7746da9e28ab1
jorgenwh <jorgenwhen@gmail.com>
2024-06-11 18:22:14 +0200

    fixing aids

--- merge_annotations.py
+++ merge_annotations.py
@@ -1,9 +1,11 @@
 import os
+
 def combine_files(input_directory, output_file):
     dataPoints = 0
     if not os.path.isdir(input_directory):
         print(f"The directory {input_directory} does not exist.")
         return    
+
     print(f"Combining files from directory: {input_directory}")
     
     with open(output_file, 'wb') as outfile:
@@ -21,6 +23,7 @@ def combine_files(input_directory, output_file):
     
     print(f"\nProcessed {dataPoints} data points.")
     print(f"All files have been combined into {output_file}")
+
 # Define the input directory and output file
 input_directory = 'training/annotations'
 output_file = 'combined_annotations.fen'
